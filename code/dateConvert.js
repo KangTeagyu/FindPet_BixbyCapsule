7f7443332217e4af9e9b489c839f3d0ba96fc7be
hyeonseo <eric71804122@gmail.com>
2019-10-27 20:00:57 +0900

    날짜 더하기, 동물 종류 선택 오류 해결

--- code/dateConvert.js
+++ code/dateConvert.js
@@ -16,9 +16,11 @@ module.exports.function = function date_Convert (myDate_Start, myDate_End){
   }
   //var bgnde_convert = InputDate['bgnde_convert']
   //var endde_convert = InputDate['endde_convert']
-  var Bgnde_Convert = String(myDate_Start['year']) + String(myDate_Start['month']) + String(myDate_Start['day'])
-  var Endde_Convert = String(myDate_End['year']) + String(myDate_End['month']) + String(myDate_End['day'])
   
+  var Bgnde_Convert = String(myDate_Start['year']) + String(myDate_Start['month']) + String(myDate_Start['day'])
+  var Newdate = new Date(String(myDate_Start['year']),String(myDate_Start['month']),String(myDate_Start['day']))
+  Newdate.setDate(Newdate.getDate()+7)
+  var Endde_Convert = String(Newdate.getFullYear()) + String(("0"+Newdate.getMonth()).slice(-2)) + String(("0"+Newdate.getDate()).slice(-2))
   return {bgnde_Convert: Bgnde_Convert,
           endde_Convert: Endde_Convert
          };
