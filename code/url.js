62f5b2faeb9f2a3fe2c1fd162bde2655d1ed4323
unknown <eric718104122@gmail.com>
2019-10-28 19:24:58 +0900

    지도 어플 연동 생성

---
+++ code/url.js
@@ -0,0 +1,5 @@
+module.exports.function = function url1 (happenPlace) {
+  //var Slicing = happenPlace.replace(/ /gi,'%')
+  var URL1 = "https://map.kakao.com/?q=" + happenPlace
+  return URL1
+}
---
+++ code/url2.js
@@ -0,0 +1,5 @@
+module.exports.function = function url2 (careAddr) {
+  //var Slicing = happenPlace.replace(/ /gi,'%')
+  var URL2 = "https://map.kakao.com/?q=" + careAddr
+  return URL2
+}
