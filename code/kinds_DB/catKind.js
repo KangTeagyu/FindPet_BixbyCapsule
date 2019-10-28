1a9211172861f03c9cac22d52958d5192ada70f3
unknown <eric718104122@gmail.com>
2019-10-28 22:05:31 +0900

    커밋 꼬임 수정

---
+++ code/kinds_DB/catKind.js
@@ -0,0 +1,137 @@
+module.exports.function = [
+  {
+    "name": "cat",
+    "kind": '000116'
+  },
+  {
+    "name": "Other",
+    "kind": '000201'
+  },
+  {
+    "name": "Norwegian Forest",
+    "kind": '000170'
+  },
+  {
+    "name": "Devon Rex",
+    "kind": '000171'
+  },
+  {
+    "name": "Russian blue",
+    "kind": '000172'
+  },
+  {
+    "name": "Legdol-Lagar Muffin",
+    "kind": '000173'
+  },
+  {
+    "name": "Manx",
+    "kind": '000174'
+  },
+  {
+    "name": "Munchkin",
+    "kind": '000175'
+  },
+  {
+    "name": "Maine Coon",
+    "kind": '000176'
+  },
+  {
+    "name": "Balinese",
+    "kind": '000177'
+  },
+  {
+    "name": "Berman",
+    "kind": '000178'
+  },
+  {
+    "name": "Bengal",
+    "kind": '000179'
+  },
+  {
+    "name": "Bombay",
+    "kind": '000180'
+  },
+  {
+    "name": "British Shorthair",
+    "kind": '000181'
+  },
+  {
+    "name": "Savannah Kat",
+    "kind": '000182'
+  },
+  {
+    "name": "Chartlux",
+    "kind": '000183'
+  },
+  {
+    "name": "Siamese",
+    "kind": '000184'
+  },
+  {
+    "name": "Selkirk Rex",
+    "kind": '000185'
+  },
+  {
+    "name": "Somali",
+    "kind": '000186'
+  },
+  {
+    "name": "Snow shoe",
+    "kind": '000187'
+  },
+  {
+    "name": "Scottish Fold",
+    "kind": '000188'
+  },
+  {
+    "name": "sphinx",
+    "kind": '000189'
+  },
+  {
+    "name": "Siberian Forest",
+    "kind": '000190'
+  },
+  {
+    "name": "Singaporepura",
+    "kind": '000191'
+  },
+  {
+    "name": "American Shorthair",
+    "kind": '000192'
+  },
+  {
+    "name": "Abyssinian",
+    "kind": '000193'
+  },
+  {
+    "name": "Japanese Rice Tail",
+    "kind": '000194'
+  },
+  {
+    "name": "Turkish Angora",
+    "kind": '000195'
+  },
+  {
+    "name": "Tonkinese",
+    "kind": '000196'
+  },
+  {
+    "name": "Persian-Persian Chinchilla",
+    "kind": '000197'
+  },
+  {
+    "name": "Havana Brown",
+    "kind": '000198'
+  },
+  {
+    "name": "Highland Fold",
+    "kind": '000199'
+  },
+  {
+    "name": "Korean cat",
+    "kind": '000200'
+  }
+]
+
+
+
